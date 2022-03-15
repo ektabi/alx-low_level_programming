8a3997ed9032a7c1815a152a6facb95115492d81
ektabi <tabiscose@gmail.com>
2022-03-15 08:55:47 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ *
+ *   */
+
+void print_alphabet(void)
+
+{
+
+		char ch;
+
+
+
+			ch = 'a';
+
+
+
+				while (ch <= 'z')
+
+						{
+
+									_putchar(ch);
+
+											ch++;
+
+												}
+
+
+
+					_putchar('\n');
+
+}
+
+
+
+    © 2022 GitHub, Inc.
+
+
+
+        Terms
