2bedd1e1dc5535ded1194916e43d55b351d0f863
ektabi <tabiscose@gmail.com>
2022-03-21 05:36:27 -0700

    readme

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
-#include <stdio.h>
+nclude < stdio.h >
 
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - prints the phrase "Programming is like building a multilingual puzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
