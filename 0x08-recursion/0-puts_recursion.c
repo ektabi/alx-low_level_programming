523fa81330565c7b51c8e19a4164ffccff8e53f8
ektabi <tabiscose@gmail.com>
2022-03-30 21:44:26 -0700

    prints a stringh

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,19 +4,13 @@
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to print
  */
-
 void _puts_recursion(char *s)
-  
 {
-  
-  if (*s == '\0')
-    
-    {
-      _putchar('\n');
-      return;
-      
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
-  
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
