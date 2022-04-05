3d2c2a4d54dac0f775c5658ae0e9243cfe6f0f1f
ektabi <tabiscose@gmail.com>
2022-04-05 01:15:13 -0700

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,9 +4,9 @@
 /**
  * *str_concat - concatenates two strings
  * @s1: string to concatenate
- *  @s2: other string to concatenate
+ * @s2: other string to concatenate
  *
- *  Return: pointer to the new string created (Success), or NULL (Error)
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -33,9 +33,17 @@ char *str_concat(char *s1, char *s2)
 			i++;
 		}
 	}
+	
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
 	s3[i] = '\0';
-
-
 
 	return (s3);
 }
